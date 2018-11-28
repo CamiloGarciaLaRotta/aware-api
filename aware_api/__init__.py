d0ebb299107f290d28dbb794381b5458e5eadfe9
cegal <camilo.garcia.larotta@gmail.com>
2018-11-27 22:34:30 -0500

    Add /api/process payload validation

--- aware_api/__init__.py
+++ aware_api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 import logging
+import json
 import os
 
 def create_app(cfg=None):
@@ -24,15 +25,31 @@ def create_app(cfg=None):
     def routes_get():
         '''
         Process the face image receied in the payload and return awareness characteristics
+        Payload: {"id":<user_id>, "image":<base64>}
         '''
         if not request.data:
             missing_payload_message = 'malformed POST request: missing payload'
             logger.warn(missing_payload_message)
             error = {'status' : 'error', 'error': missing_payload_message}
-            res =  jsonify(error), 400
-            return res
+            return jsonify(error), 400
 
-        return jsonify({'status': 'ok'}), 200
+        payload = json.loads(request.data)
+
+        if 'id' not in payload:
+            missing_id_message = 'malformed POST request: missing id'
+            logger.warn(missing_id_message)
+            error = {'status' : 'error', 'error': missing_id_message}
+            return jsonify(error), 400
+        if 'image' not in payload:
+            missing_image_message = 'malformed POST request: missing image'
+            logger.warn(missing_image_message)
+            error = {'status' : 'error', 'error': missing_image_message}
+            return jsonify(error), 400
+
+        id = payload['id']
+        image = payload['image']
+
+        return jsonify({'status': 'ok', 'id': id, 'image': image}), 200
 
     return app
 
